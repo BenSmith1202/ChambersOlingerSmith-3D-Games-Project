71f355a080841c67501f83d1bacfa65ebc037cef
Michael Chambers <geofreypillagefish@gmail.com>
2025-03-21 13:47:23 -0400

    started work on melee officer movement

--- Assets/Enemies/TimePolice/MeleeOfficer/MeleeOfficerScript.cs
+++ Assets/Enemies/TimePolice/MeleeOfficer/MeleeOfficerScript.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
+using UnityEngine.InputSystem.HID;
 
 public class MeleeOfficerScript : MonoBehaviour
 {
     [SerializeField] GameObject target;
 
-    [SerializeField] float meleeRange = 1;
     [SerializeField] int angleCheckDelta = 30;
+
+    [SerializeField] float minWalkTimeLimit = 2;
+    [SerializeField] float maxWalkTimeLimit = 5;
+
+    [SerializeField] float visibilityDistance = 30;
+    [SerializeField] float meleeRange = 1;
 
     Rigidbody rb;
     Vector3 directionToTarget;
     EntityStats es;
+
+    enum Range
+    {
+        unaware, aware, inRange
+    }
+    Range decisionRange;
 
     private void Start()
     {
@@ -21,21 +34,75 @@ public class MeleeOfficerScript : MonoBehaviour
         {
             target = GameObject.Find("Player");
         }
-        getFarthestDistanceDirection();
+
+        decisionRange = Range.unaware;
+        StartCoroutine(Behavior());
     }
 
     private void FixedUpdate()
     {
-        //try to get to target
-        if(Vector3.Distance(target.transform.position, transform.position) < meleeRange)
+        if(Vector3.Distance(target.transform.position, transform.position) > visibilityDistance)
         {
-            //attack
+            decisionRange = Range.unaware;
+        }
+        else if(Vector3.Distance(target.transform.position, transform.position) > meleeRange)
+        {
+            decisionRange = Range.aware;
         }
         else
         {
-            directionToTarget = (target.transform.position - transform.position).normalized;
-            rb.velocity = new Vector3(directionToTarget.x * es.baseSpeed, rb.velocity.y, directionToTarget.z * es.baseSpeed);
+            decisionRange = Range.inRange;
         }
+    }
+
+    IEnumerator Behavior()
+    {
+        while(true)
+        {
+            yield return StartCoroutine(Meander());
+        }
+    }
+
+    IEnumerator Meander()
+    {
+        yield return StartCoroutine(RotateTowardsDirection(GetFarthestDistanceDirection()));
+        while(decisionRange == Range.unaware)
+        {
+            yield return StartCoroutine(MoveForward());
+        }
+    }
+
+    IEnumerator RotateTowardsDirection(Vector3 direction)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 500 * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+    }
+
+    IEnumerator MoveForward()
+    {
+        float wallCheckDistance = 3;
+        RaycastHit hit;
+        Vector3 forward = transform.forward;
+
+        float maxWalkTime = Time.time + Random.Range(minWalkTimeLimit, maxWalkTimeLimit);
+        while (!Physics.Raycast(transform.position, forward, out hit, wallCheckDistance) && Time.time < maxWalkTime)
+        {
+            if(decisionRange != Range.unaware)
+            {
+                yield break;
+            }
+            Debug.DrawRay(transform.position, forward * wallCheckDistance, Color.blue, 0.1f);
+            rb.velocity = new Vector3(transform.forward.normalized.x * es.baseSpeed, rb.velocity.y, transform.forward.normalized.z * es.baseSpeed);
+            yield return null;
+        }
+        yield return StartCoroutine(RotateTowardsDirection(GetFarthestDistanceDirection()));
     }
 
     /*
@@ -43,7 +110,7 @@ public class MeleeOfficerScript : MonoBehaviour
      * it picks the longest ray
      * this ray direction is the nicest direction to walk towards for the enemy 
      */
-    Vector3 getFarthestDistanceDirection()
+    Vector3 GetFarthestDistanceDirection()
     {
         Vector3 origin = transform.position;
         float maxDistance = 100;
@@ -68,19 +135,19 @@ public class MeleeOfficerScript : MonoBehaviour
 
         for(int i = angleCheckDelta; i <= 360; i += angleCheckDelta)
         {
-            Debug.DrawRay(origin, Quaternion.Euler(0, angleCheckDelta, 0) * forward * maxDistance, Color.red, 10f);
-            if (Physics.Raycast(origin, Quaternion.Euler(0, angleCheckDelta, 0) * forward, out hit, maxDistance))
+            Debug.DrawRay(origin, Quaternion.Euler(0, i, 0) * forward * maxDistance, Color.red, 10f);
+            if (Physics.Raycast(origin, Quaternion.Euler(0, i, 0) * forward, out hit, maxDistance))
             {
                 if(hit.distance > maxDistanceFound)
                 {
                     maxDistanceFound = hit.distance;
-                    bestDirection = Quaternion.Euler(0, angleCheckDelta, 0) * forward;
+                    bestDirection = Quaternion.Euler(0, i, 0) * forward;
                 }
             }
             else
             {
                 maxDistanceFound = maxDistance;
-                bestDirection = Quaternion.Euler(0, angleCheckDelta, 0) * forward;
+                bestDirection = Quaternion.Euler(0, i, 0) * forward;
                 return bestDirection;
             }
         }
