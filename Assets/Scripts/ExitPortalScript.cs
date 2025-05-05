77cfd85ed15faef39146c75351d2d3ca87557602
DavidOlinger <126510514+DavidOlinger@users.noreply.github.com>
2025-05-05 07:40:59 -0400

    push

--- Assets/Scripts/ExitPortalScript.cs
+++ Assets/Scripts/ExitPortalScript.cs
@@ -22,9 +22,18 @@ public class ExitPortalScript : MonoBehaviour
     {
         if (logicManager.objectiveComplete && collision.gameObject.GetComponent<PlayerControllerScript>() != null)
         {
-            collision.gameObject.GetComponent<EntityStats>()?.LevelUp(1);
-            LevelManager.Instance.LoadNextLevel();
+            //collision.gameObject.GetComponent<EntityStats>().LevelUp(1);
+            PlayerSavingScript savingScript = GameObject.FindWithTag("Player").GetComponent<PlayerSavingScript>();
+            GameObject.FindWithTag("Player").GetComponent<EntityStats>().level += 1;
+            print(GameObject.FindWithTag("Player").GetComponent<EntityStats>().level);
+            savingScript.SavePlayerToFile();
+            Invoke("LD", 0.2f);
         }
 
+    }
+
+    private void LD()
+    {
+        LevelManager.Instance.LoadNextLevel();
     }
 }
